dd4c2ca2864a6febb1f524c1f83873c6ce6af54b
Sergio Quijano <sergiquijano@gmail.com>
2019-02-25 19:23:25 +0100

    Practica 1 Ruby Añado fichero ruby para Weapon

---
+++ deepspace/Weapon.rb
@@ -0,0 +1,45 @@
+#encoding:utf-8
+
+# Class to represent the weapons that a space station can dispose of to
+# increment its energy when shooting
+class Weapon
+
+	def initialize(_name, _type, _uses)
+		@name = _name
+		@type = _type
+		@uses = _uses
+	end
+
+	def newCopy(origin)
+		return Weapon.new(@origin.name, @origin.type, @origin.uses)
+	end
+
+	# Description:
+	# 	Getter for type
+	# Return
+	#	WeaponType with type
+	def type
+		return @type
+	end
+
+	# Description:
+	# 	Getter for uses
+	# Returns:
+	#	Int with uses
+	def uses
+		return @uses
+	end
+
+	def power
+		return @type.power
+	end
+
+	def useIt
+		if @uses > 0
+			@uses -= 1
+			return power
+		else
+			return 1.0
+		end
+	end
+end
