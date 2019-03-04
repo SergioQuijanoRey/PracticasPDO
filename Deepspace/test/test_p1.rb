894bde467ebd74503d0da621450c0c156575f483
Sergio Quijano <sergiquijano@gmail.com>
2019-03-04 21:13:34 +0100

    Añado dudas que tengo para el profesor Moldeo un poco el fichero de testeo de la primera practica

--- Deepspace/test/test_p1.rb
+++ Deepspace/test/test_p1.rb
@@ -1,4 +1,17 @@
-require_relative "./deepspace"
+#!/usr/bin/env ruby
 
-# Main program
-puts "works"
+# WIP -- Need to develop unit testing
+# WIP -- Preguntar si tenemos libertad para desarrollar los test
+
+# Class for holding main program
+class TestP1
+	def initalize
+	end
+	def main
+		puts "Works!"
+	end
+end
+
+# Tests are launched
+test = TestP1.new
+test.main()
