d2088e8d86933a94d8441413d9c48a92ff3fddf4
Sergio Quijano <sergiquijano@gmail.com>
2019-03-08 12:16:55 +0100

    Practica 1 Java Creo la clase ShieldBooster

---
+++ deepspace/src/ShieldBooster.java
@@ -0,0 +1,52 @@
+
+// WIP -- Lack of documentation
+
+public class ShieldBooster{
+	// Private attributes
+	//======================================================================
+	private String name;
+	private float boost;
+	private int uses;
+
+	// Constructors
+	//======================================================================
+	public ShieldBooster(String _name, float _boost, int _uses){
+		name = _name;
+		boost = _boost;
+		uses = _uses;
+	}
+
+	public ShieldBooster(ShieldBooster other){
+		this(other.name, other.getBoost(), other.getUses());
+	}
+
+	// Getters
+	//======================================================================
+	public float getBoost(){
+		return boost;
+	}
+
+	public int getUses(){
+		return uses;
+	}
+
+	// Setters
+	//======================================================================
+	/**
+	 * @brief method to use the shield booster
+	 * @return boost, if there are uses left
+	 * 	   1.0 (neutral boost) otherwise
+	 * */
+	public float useIt(){
+		if(uses > 1){
+			uses = uses - 1;
+			return boost;
+		}else{
+			return 1.0;
+		}
+
+		// Security check
+		System.out.println("WARNING! Unexpected condition at ShieldBooster.useIt()");
+		return 1.0;
+	}
+}
