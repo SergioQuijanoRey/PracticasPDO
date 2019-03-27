28352ee8f3110bcb9f41912b730bd2a8ff58947a
Sergio Quijano <sergiquijano@gmail.com>
2019-03-27 10:59:59 +0100

    Arreglo EnemyStarShip.rb

--- Deepspace/lib/EnemyStarShip.rb
+++ Deepspace/lib/EnemyStarShip.rb
@@ -1,78 +1,5 @@
 #encoding:utf-8
 
-<<<<<<< HEAD
-require_relative 'enums'
-require_relative 'EnemyToUI'
-
-# wip -complete docs
-class EnemyStarShip
-
-    # Constructors
-	#=======================================================================
-
-    # Description:
-    # 	Initializer of the class
-    # Parameters:
-	# 	n: String
-	# 	a: Float
-	# 	s: Float
-	# 	l: Loot
-	# 	d: Damage
-    # Return:
-    #   Nil
-    def initialize(n, a, s, l, d)
-        @name = n
-        @ammoPower = a
-        @shieldPower = s
-        @loot = l
-        @damage = d
-    end
-
-    def self.newCopy(e)
-        return EnemyStarShip.new(e.name, e.ammoPower, e.shieldPower, e.loot, e.damage)
-    end
-
-    def self.getUIversion
-        return EnemyToUI.new(self)
-    end
-
-    def fire
-        return @ammoPower
-    end
-
-    def ammoPower
-        return @ammoPower
-    end
-
-    def damage
-        return @damage
-    end
-
-    def loot
-        return @loot
-    end
-
-    def name
-        return @name
-    end
-
-    def shieldPower
-        return @shieldPower
-    end
-
-    def protection
-        return @shieldPower
-    end
-
-    def receiveShot(shot)
-        if protection < shot
-            return ShotResult::RESIST
-        else
-            return ShotResult::DONOTRESIST
-        end
-    def
-end
-=======
 require_relative "../lib/enums.rb"
 require_relative "../lib/EnemyToUI.rb"
 require_relative "../lib/Loot.rb"
@@ -170,4 +97,3 @@ class EnemyStarShip
 end
 
 end	# module Deepspace
->>>>>>> sergio
