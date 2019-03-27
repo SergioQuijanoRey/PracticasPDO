70537a8f91eb1a133f75df9e48862bf2e81316e8
mianfg <mianfg19@gmail.com>
2019-03-27 10:46:31 +0100

    Descripcion

--- Deepspace/lib/GameState.rb
+++ Deepspace/lib/GameState.rb
@@ -11,10 +11,10 @@ module GameState
     AFTERCOMBAT = :aftercombat
     
 
-#     CANNOTPLAY:     the Gameuniverse has been created but not initialized
-#     INIT:   the GameUniverse has been initialized and the current player has not combated. Players can mount weapons and shielboosters, or combat
-#     BEFORECOMBAT:   players can only combat
-#     AFTERCOMBAT:    players can mount/discard weapons,shielboosters and hangars 
+#	CANNOTPLAY:     the Gameuniverse has been created but not initialized
+#	INIT:			the GameUniverse has been initialized and the current player has not combated. Players can mount weapons and shielboosters, or combat
+#	BEFORECOMBAT:	players can only combat
+#	AFTERCOMBAT:	players can mount/discard weapons,shielboosters and hangars 
 
 end # module GameState
 
