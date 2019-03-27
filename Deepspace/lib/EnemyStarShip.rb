d9fe57db4da1c110e2a51573f409776bd7a3d3e8
mianfg <mianfg19@gmail.com>
2019-03-27 18:02:07 +0100

    Typos

--- Deepspace/lib/EnemyStarShip.rb
+++ Deepspace/lib/EnemyStarShip.rb
@@ -7,93 +7,95 @@ require_relative "../lib/Loot.rb"
 module Deepspace
 
 class EnemyStarShip
-		# Initializers
-		#=======================================================================
-		# Description:
-		# 		Initializer of the class
-		# Parameters:
-		# 		_name: String
-		# 		_ammoPower: Float
-		# 		_shieldPower: Float
-		# 		_loot: Loot
-		# 		_damage: Damage
-		def initialize(_name, _ammoPower, _shieldPower, _loot, _damage)
-				@name = _name
-				@ammoPower = _ammoPower
-				@shieldPower = _shieldPower
-				@loot = _loot
-				@damage = _damage
-		end
 
-		def self.newCopy(enemy)
-				return EnemyStarShip.new(enemy.name, enemy.ammoPower, enemy.shieldPower, enemy.loot, enemy.damage)
-		end
-		
-		# Getters
-		#=======================================================================
-		def name
-				return @name
-		end
+	# Constructors
+	#=======================================================================
 
-		def ammoPower
-				return @ammoPower
-		end
+	# Description:
+	# 	Initializer of the class
+	# Parameters:
+	#  	_name: String
+	# 	_ammoPower: Float
+	# 	_shieldPower: Float
+	# 	_loot: Loot
+	# 	_damage: Damage
+	def initialize(_name, _ammoPower, _shieldPower, _loot, _damage)
+		@name = _name
+		@ammoPower = _ammoPower
+		@shieldPower = _shieldPower
+		@loot = _loot
+		@damage = _damage
+	end
 
-		def shieldPower
-				return @shieldPower
-		end
+	def self.newCopy(enemy)
+		return EnemyStarShip.new(enemy.name, enemy.ammoPower, enemy.shieldPower, enemy.loot, enemy.damage)
+	end
+	
+	# Getters
+	#=======================================================================
+	def name
+		return @name
+	end
 
-		def loot
-				return @loot
-		end
+	def ammoPower
+		return @ammoPower
+	end
 
-		def damage
-				return @damage
-		end
+	def shieldPower
+		return @shieldPower
+	end
 
-		# Description:
-		# 	Gets UI representation of the object
-		# Returns:
-		# 	EnemyToUI: the UI representation
-		def getUIVersion
-				return EnemyToUI.new(self)
-		end
+	def loot
+		return @loot
+	end
 
-		# Description
-		# 	EnemyStarShip attacks with certain power
-		# Returns:
-		# 		Float: the damage it can do
-		def fire
-				return @ammoPower
-		end
+	def damage
+		return @damage
+	end
 
-		# Description:
-		# 	EnemyStarShip protects from one attack
-		# Returns:
-		# 	Float: the protection it takes
-		def protection
-				return @shieldPower
-		end
+	# Description:
+	# 	Gets UI representation of the object
+	# Returns:
+	# 	EnemyToUI: the UI representation
+	def getUIVersion
+		return EnemyToUI.new(self)
+	end
 
-		def to_s
-				return "EnemyStarShip(\"#{@name}\", #{@ammoPower}, #{@shieldPower}, #{@loot}, #{@damage})"
-		end
+	# Description
+	# 	EnemyStarShip attacks with certain power
+	# Returns:
+	# 	Float: the damage it can do
+	def fire
+		return @ammoPower
+	end
 
-		# Setters
-		#=======================================================================
-		# Parameters
-		# 	shot: Float, the power of taken shot
-		# Returns:
-		# 	ShotResult:	ShotResult::RESIST, if shieldPower >= shot
-		# 				ShotResult::DONOTRESIST, if shieldPower = shot
-		def receiveShot(shot)
-				if @shieldPower >= shot
-						return ShotResult::RESIST
-				else
-						return ShotResult::DONOTRESIST
-				end
-		end
+	# Description:
+	# 	EnemyStarShip protects from one attack
+	# Returns:
+	# 	Float: the protection it takes
+	def protection
+		return @shieldPower
+	end
 
-end
+	def to_s
+		return "EnemyStarShip(\"#{@name}\", #{@ammoPower}, #{@shieldPower}, #{@loot}, #{@damage})"
+	end
+
+	# Description:
+	# 	Receives a certain shot
+	# Parameters:
+	# 	shot: Float, the power of shot taken
+	# Returns:
+	# 	ShotResult:	ShotResult::RESIST, if shieldPower >= shot
+	# 				ShotResult::DONOTRESIST, if shieldPower = shot
+	def receiveShot(shot)
+		if @shieldPower >= shot
+			return ShotResult::RESIST
+		else
+			return ShotResult::DONOTRESIST
+		end
+	end
+
+end # class EnemyStarShip
 
 end	# module Deepspace
