a7320ff7a439a97f21f504bcdcbe79f3d8f18198
mianfg <mianfg19@gmail.com>
2019-03-27 18:00:37 +0100

    Arreglada typo, modificado declaración de private, expresiones array Ruby, cambios de -=, +=

--- Deepspace/lib/Damage.rb
+++ Deepspace/lib/Damage.rb
@@ -18,18 +18,18 @@ class Damage
 	# 	_nShields: Integer, number of lost shields
 	# 	_weapons: WeaponType[], types of weapons to be discarded
 	def initialize(_nWeapons, _nShields, _weapons)
-			@nWeapons = _nWeapons
-			@nShields = _nShields
-			@weapons = _weapons
+		@nWeapons = _nWeapons
+		@nShields = _nShields
+		@weapons = _weapons
 	end
 	
 	# Description:
 	# 	Creates a Damage where weapon types are not known
 	# Parameters:
-	# 	_nWeawpons: Integer, number of weapons to loose
+	# 	_nWeapons: Integer, number of weapons to loose
 	# 	_nShields: Integer, number of shields to loose
 	def self.newNumericWeapons(_nWeapons, _nShields)
-			return new(_nWeapons, _nShields, [])
+		return new(_nWeapons, _nShields, [])
 	end
 
 	# Description:
@@ -38,7 +38,7 @@ class Damage
 	# 	_weapons: WeaponType[], types of weapons to discard
 	# 	_nShields: Integer, number of shields to loose
 	def self.newSpecificWeapons(weapons, _nShields)
-			return new(0, _nShields, weapons)
+		return new(0, _nShields, weapons)
 	end
 
 	# Description:
@@ -48,25 +48,25 @@ class Damage
 	# Returns
 	# 	Damage: a copy of the given instance
 	def self.newCopy(d)
-			if d.weapons.empty?
-					return newNumericWeapons(d.nWeapons, d.nShields)
-			else
-					return newSpecificWeapons(d.weapons, d.nShields)
-			end
+		if d.weapons.empty?
+			return newNumericWeapons(d.nWeapons, d.nShields)
+		else
+			return newSpecificWeapons(d.weapons, d.nShields)
+		end
 	end
 
 	# Getters
 	#=======================================================================
 	def nWeapons
-			return @nWeapons
+		return @nWeapons
 	end
 
 	def nShields
-			return @nShields
+		return @nShields
 	end
 
 	def weapons
-			return @weapons
+		return @weapons
 	end
 
 	# Description:
@@ -74,7 +74,7 @@ class Damage
 	# Returns:
 	# 	DamageToUI: the UI representation of the object
 	def getUIVersion
-			return DamageToUI.new(self)
+		return DamageToUI.new(self)
 	end
 
 	# Description:
@@ -85,27 +85,27 @@ class Damage
 	# Returns:
 	# 	Int:	position, if the element is found
 	# 			-1, if no element is found
-	private def arrayContainsType(w, t)
-			for index in 0..w.length-1
-					# Fist position found
-					if w[index] == t
-							return index
-					end
+	def arrayContainsType(w, t)
+		for index in 0..w.length-1
+			# Fist position found
+			if w[index] == t
+				return index
 			end
+		end
 
-			# No element found
-			return -1
+		# No element found
+		return -1
 	end
 
 	def to_s
-			if @weapons.empty?
-					return "Damage.newNumericWeapons(#{@nWeapons}, #{@nShields})"
-			else
-					return "Damage.newSpecificWeapons(#{@weapons}, #{@nShields})"
-			end
+		if @weapons.empty?
+			return "Damage.newNumericWeapons(#{@nWeapons}, #{@nShields})"
+		else
+			return "Damage.newSpecificWeapons(#{@weapons}, #{@nShields})"
+		end
 
-			# Security condition
-			return ""
+		# Security condition
+		return ""
 	end
 	
 	# Setters
@@ -113,76 +113,82 @@ class Damage
 	# Description:
 	# 	Creates a copy of current objet where weapons and shields which are
 	# 	not included in arrays given as parameters are discarded. That's to say,
-	# 	we srink the Damage to the parameters
+	# 	we shrink the Damage to the parameters
 	# Parameters:
 	# 	w: Weapon[], weapons to fit
 	# 	s: shieldBooster[], shields to fit
 	# Returns:
 	# 	Damage: a copy of the object adjusted as explained above
-	# WIP -- not finished
 	def adjust(w, s)
-			if @weapons.empty? == false
-					# Intersection is computed
-					new_weapons = []
-					for weapon in w
-							if @weapons.include? weapon
-									new_weapons.push(weapon)
-							end
-					end
-
-					# New object is created
-					copy = Damage.newSpecificWeapons(new_weapons, @nShields)
-			else
-					puts "WARNING! No specific weapons to adjust, at Damage.adjust()"
-					return Nil
+		if @weapons.empty? == false
+			# Intersection is computed
+			new_weapons = []
+			for weapon in w
+				if @weapons.include? weapon
+					new_weapons << weapon
+				end
 			end
+
+			# New object is created
+			copy = Damage.newSpecificWeapons(new_weapons, @nShields)
+		else
+			puts "WARNING! No specific weapons to adjust, at Damage.adjust()"
+			return Nil
+		end
 	end
 
 	# Description:
 	# 	Removes a given type of weapon
-	# 	If a list of weapon types is not available, the number of weapons to be deleted decreases by 1
+	# 	If a list of weapon types is not available, the number of weapons to be
+	# 	deleted decreases by 1
 	# Parameters:
-	# 	w: WeaponType, the weapon type to be removed # WIP -- No concuerda con el guion de la practica!
+	# 	w: Weapon, the weapon to be removed
+	# Return:
+	# 	Nil
+	# WIP Hace falta hacer más comprobaciones sobre el arma??
 	def discardWeapon(w)
-			if @weapons.length != 0
-					position = arrayContainsType(weapons, w)
-					if position != -1
-							@weapons.delete_at(position)
-					else
-							puts "WARNING! No weapon type match at Damage.discardWeapon()"
-					end
-
+		if @weapons.length != 0
+			position = arrayContainsType(weapons, w.type)
+			if position != -1
+				@weapons.delete_at(position)
 			else
-					if @nWeapons > 0
-							@nWeapons = @nWeapons - 1
-					else
-							puts "WARNING! You tryied to have negative weapons at Damage.discardWeapon()"
-					end
+				puts "WARNING! No weapon type match at Damage.discardWeapon()"
 			end
+
+		else
+			if @nWeapons > 0
+				@nWeapons -= 1
+			#else
+			#	puts "WARNING! You attempted to have negative weapons at Damage.discardWeapon()"
+			end
+		end
 	end
 
 	# Description:
 	# 	Reduces by 1 the number of shieldBoosters to be removed
+	# Return:
+	# 	Nil
 	def discardShieldBooster
-			if @nShields > 0
-					@nShields = @nShields - 1
-			else
-					puts "WARNING! You tryied to have negative shieldBoosters at Damage.discardShieldBooster()"
-			end
+		if @nShields > 0
+			@nShields -= 1
+		#else
+		#	puts "WARNING! You tried to have negative shieldBoosters at Damage.discardShieldBooster()"
+		end
 	end
 
 	# Description:
 	# 	Checks wether or not damage is affecting
 	# Returns:
-	# 	Boolean:	true, if damage has effective effect
-	# 				false, if damage has no effective effect
+	# 	Bool:	true, if damage has no effect
+	# 			false, if damage has effect
 	def hasNoEffect
-			return @nShields + @nWeapons > 0
+			return @nShields + @nWeapons == 0
 	end
 
 	# Visibility specifiers
 	#=======================================================================
 	private_class_method :new
+	private :arrayContainsType
 end
 
 end	# module Deepspace
