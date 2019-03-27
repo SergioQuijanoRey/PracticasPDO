7b82cabcce13b8c9700cd353e5e8e8c89ab11cae
Sergio Quijano <sergiquijano@gmail.com>
2019-03-27 12:06:11 +0100

    Añado las implementaciones de los metodos especificados en el guion Quedan metodos sin implementar, esto se tiene que hacer en la tercera pratica

--- Deepspace/lib/GameUniverse.rb
+++ Deepspace/lib/GameUniverse.rb
@@ -29,6 +29,7 @@ class GameUniverse
 				return @gameState
 		end
 
+		# WIP -- Practica 3
 		# Description:
 		# Paramters:
 		# 	station: SpaceStation
@@ -38,6 +39,7 @@ class GameUniverse
 		def combatGo(station, enemy)
 		end
 
+		# WIP -- Practica 3
 		# Description:
 		# Returns:
 		# 	CombatResult
@@ -49,59 +51,107 @@ class GameUniverse
 		end
 
 		# Description:
+		# 	Checks if spaceShip which is on turn have won
 		# Returns:
-		# 	Boolean
+		# 	Boolean	true, if the space ship has won
+		# 			false, otherwise
 		def haveAWinner
+				if @currentStation.nMedals >= @@WIN
+						return true
+				else
+						return false
+				end
+		end
+
+		def to_s
 		end
 
 		# Setters
 		#=======================================================================
 		
+		# Description:
+		# 	The current space station discards an Hangar if GameState is INIT or AFTERCOMBAT
+		# 	Otherwise, this method has no effect
 		def discardHangar
+				if @gameState == GameState::INIT or GameState::AFTERCOMBAT
+						@currentStation.discardHangar
+				end
 		end
 
 		# Description:
+		# 	The current space station discards a shield Booster if GameState is INIT or AFTERCOMBAT
+		# 	Otherwise, this method has no effect
 		# Parameters:
-		# 	i: Integer
+		# 	i: Integer, position of the shield booster to discard
 		def discardShieldBooster(i)
+				if @gameState == GameState::INIT or GameState::AFTERCOMBAT
+						@currentStation.discardShieldBooster(i)
+				end
 		end
 
 		# Description:
+		# 	The current space station discards a shield Booster in the Hangar if GameState is INIT or AFTERCOMBAT
+		# 	Otherwise, this method has no effect
 		# Parameters:
-		# 	i: Integer
+		# 	i: Integer, position of the shield booster to discard
 		def discardShieldBoosterInHangar(i)
+				if @gameState == GameState::INIT or GameState::AFTERCOMBAT
+						@currentStation.discardShieldBoosterInHangar(i)
+				end
 		end
 
 		# Description:
+		# 	The current space station discards a weapon if GameState is INIT or AFTERCOMBAT
+		# 	Otherwise, this method has no effect
 		# Parameters:
-		# 	i: Integer
+		# 	i: Integer, position of the weapon to discard
 		def discardWeapon(i)
+				if @gameState == GameState::INIT or GameState::AFTERCOMBAT
+						@currentStation.discardWeapon(i)
+				end
 		end
 
 		# Description:
+		# 	The current space station discards a weapon in the hangar if GameState is INIT or AFTERCOMBAT
+		# 	Otherwise, this method has no effect
 		# Parameters:
-		# 	i: Integer
+		# 	i: Integer, position of the weapon to discard
 		def discardWeaponInHangar(i)
+				if @gameState == GameState::INIT or GameState::AFTERCOMBAT
+						@currentStation.discardWeaponInHangar(i)
+				end
 		end
 
 		# Description:
+		# 	The current space station mounts a shield booster if GameState is INIT or AFTERCOMBAT
+		# 	Otherwise, this method has no effect
 		# Parameters:
-		# 	i: Integer
+		# 	i: Integer, position of the shield booster to mount
 		def mountShieldBooster(i)
-		end
-		
-		# Description:
-		# Parameters:
-		# 	i: Integer
-		def mountWeapon(i)
+				if @gameState == GameState::INIT or GameState::AFTERCOMBAT
+						@currentStation.mountShieldBooster(i)
+				end
 		end
 
+		# Description:
+		# 	The current space station mounts a shield booster if GameState is INIT or AFTERCOMBAT
+		# 	Otherwise, this method has no effect
+		# Parameters:
+		# 	i: Integer, position of the weapon to mount
+		def mountWeapon(i)
+				if @gameState == GameState::INIT or GameState::AFTERCOMBAT
+						@currentStation.mountWeapon(i)
+				end
+		end
+
+		# WIP -- Practica 3
 		# Description:
 		# Returns:
 		# 	Boolean
 		def nexTurn
 		end
 
+		# WIP -- Practica 3
 		# Description:
 		# Parameters:
 		# 	names: String[]
