723e9d088835c9b5a68f192014fab2d7e49d59c8
Sergio Quijano Rey <sergiquijano@gmail.com>
2019-05-30 17:38:21 +0200

    Añado los archivos de Testeo, con los fallos que genere tengo que ir haciendo merge de las correcciones que he ido haciendo

--- deepspace/src/deepspace/TestP2.java
+++ deepspace/src/deepspace/TestP2.java
@@ -1,219 +1,156 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/**
+ * @author Sergio Quijano Rey
+ * @file TestP2.java
+ * @brief Tests for all the code implemented at the second session
+ * */
 
 package deepspace;
 
-import java.util.Random;
+import java.util.ArrayList;
 
-/**
- *
- * @author Miguel Ángel Fernández Gutiérrez, Sergio Quijano Rey
- */
-public class TestP2 {
-    
-    private static Random rand = new Random();
-    
-    public static void puts(String message) {
-        System.out.println(message);
-    }
-    
-    public static void main(String[] args) {
-        puts("TEST CLASS: Hangar");
-        puts("================================================================================");
-	for ( int i = 0; i < 2; i++ ) {
-            int maxElements = rand.nextInt(10);
-            puts("Creating Hangar("+maxElements+")");
-            Hangar hangar_test = new Hangar(maxElements);
-            puts("Result: " + hangar_test);
-            puts("Adding seven different weapons");
-            for ( int j = 0; j < 7; j++ ) {
-                String name = "certainWeapon" + j;
-                WeaponType[] types = {WeaponType.LASER, WeaponType.MISSILE, WeaponType.PLASMA};
-                WeaponType type = types[rand.nextInt(3)];
-                int uses = rand.nextInt(10);
-                Weapon weapon = new Weapon(name, type, uses);
-                puts("Trying to add: " + weapon.toString());
-                if ( hangar_test.addWeapon(weapon) == false )
-                    puts("\t"+name+" cannot be added on iteration "+j);
-                else
-                    puts("\t"+name+" can be added on iteration "+j);
+class TestP2{
+    public static void main(String[] args){
+        // Testing hangar
+        //======================================================================
+        System.out.println("Testing Hangar");
+        System.out.println("================================================================================");
+       
+        // Creating an hangar
+        Hangar test_hangar = new Hangar(5);
+        System.out.println("The created hangar is " + test_hangar.toString());
+
+        // Creating a copy
+        Hangar test_hangar_copy = new Hangar(test_hangar);
+        System.out.println("The copy of the previous hangar is " + test_hangar_copy.toString());
+
+        // Testing the UI Version 
+        HangarToUI test_hangar_ui = test_hangar.getUIversion();
+        System.out.println("The hangar UI Version is " + test_hangar_ui.toString());
+
+        // Testing add weapon and add shield
+        Weapon test_weapon = new Weapon("test weapon", WeaponType.LASER, 2);
+        ShieldBooster test_shield = new ShieldBooster("test shield", (float)2.3123, 4);
+
+        System.out.println("Adding some weapons and shields");
+        for(int i = 0; i < 3; i++){
+            if(test_hangar.addWeapon(test_weapon)){
+                System.out.println("Weapon added succesfully to test_weapon");
+            }else{
+                System.out.println("Weapon failed to add to test_weapon");
             }
-            
-            puts("Hangar state is: " + hangar_test.toString());
-            puts("Removing two weapons");
-            for ( int j = 0; j < 2; j++ ) {
-                Weapon removed_weapon = hangar_test.removeWeapon(0);
-                puts("\tWeapon removed: " + removed_weapon.toString());
+        }
+
+        for(int i = 0; i < 3; i++){
+            if(test_hangar.addShieldBooster(test_shield)){
+                System.out.println("ShieldBooster added succesfully to test_weapon");
+            }else{
+                System.out.println("ShieldBooster failed to add to test_weapon");
             }
-            
-            puts("Hangar state is:\n\t" + hangar_test.toString());
-            
-            puts("Adding seven different shield boosters");
-            
-            for ( int j = 0; j < 7; j++ ) {
-                String name = "certainShieldBooster" + j;
-                float boost = rand.nextInt(9) + rand.nextFloat();
-                int uses = rand.nextInt(10);
-                ShieldBooster shield = new ShieldBooster(name, boost, uses);
-                puts("Trying to add booster: " + shield.toString());
-                if ( hangar_test.addShieldBooster(shield) ==  false )
-                    puts("\t" + name + " cannot be added on iteration " + j);
-                else
-                    puts("\t" + name + " can be added on iteration " + j);
-            }
-            
-            puts("Hangar state is:\n\t" + hangar_test.toString());
-            
-            puts("Removing two shields");
-            
-            for ( int j=0; j < 2; j++ ) {
-                ShieldBooster removed_shield = hangar_test.removeShieldBooster(0);
-                puts("\tShield removed: "+removed_shield.toString());
-            }
-            
-            puts("Testing getters:");
-			puts("\tWeapons: " + hangar_test.getWeapons().toString());
-			puts("\tShields: "+hangar_test.getShieldBoosters().toString());
-			puts("\tMax elements: "+hangar_test.getMaxElements());
-			puts("--------------------------------------------------------------------------------");
-            }
-    }
-                
-/*
-		puts "TEST CLASS: Damage"
-		puts "================================================================================"
+        }
 
-		3.times do
-			puts "Creating both types of damage..."
-			nWeapons = rand(10)
-			nShields = rand(10)
-			puts "Creating Damage.newNumericWeapons(#{nWeapons}, #{nShields})"
-			numeric_damage_test = Damage.newNumericWeapons(nWeapons, nShields)
-			weapons = []
-			4.times do
-				weapons << [WeaponType::LASER, WeaponType::MISSILE, WeaponType::PLASMA][rand(3)]
-			end
-			nShields = rand(10)
-			puts "Creating Damage.newSpecificWeapons(#{weapons}, #{nShields})"
-			type_damage_test = Damage.newSpecificWeapons(weapons, nShields)
+        // We show that the copy is different from the original
+        System.out.println("State of the original hangar: " + test_hangar.toString());
+        System.out.println("State of the copy: " + test_hangar_copy.toString());
 
-			puts "Numeric Damage: \n\t#{numeric_damage_test}"
-			puts "Specific damage: \n\t#{type_damage_test}"
-			puts ""
+        // We remove a few weapons
+        System.out.println("Removing two weapons");
+        for(int i = 0; i < 2; i++){
+            test_hangar.removeWeapon(0);
+        }
 
-			puts "Creating a copy of Numeric Damage"
-			copy_test = Damage.newCopy(numeric_damage_test)
-			puts "Copy Damage: \n\t#{copy_test}"
-			puts ""
+        System.out.println("Removing two shields");
+        for(int i = 0; i < 2; i++){
+            test_hangar.removeShieldBooster(0);
+        }
 
-			puts "Creating UI version"
-			ui_damage_test = numeric_damage_test.getUIVersion
-			puts "UI version: #{ui_damage_test}"
-			puts ""
+        System.out.println("State of the original hangar: " + test_hangar.toString());
+        System.out.println("State of the copy: " + test_hangar_copy.toString());
 
-			#WIP mianfg -- he llegado hasta aquí
-			puts "Adjusting the Specific Damage"
-			srink_damage_test = type_damage_test.adjust([WeaponType::LASER, WeaponType::MISSILE], ["test", "this does nothing"])
-			puts "\tAdjusted Damage: #{srink_damage_test}"
-			puts "\tAdjusted Damage Weapons: #{srink_damage_test.weapons}"
-			puts "" 
+        System.out.println("");
+        System.out.println("");
 
-			puts "Testing hasNoEffect"
-			puts "Has no effect: #{numeric_damage_test.hasNoEffect}"
-			puts ""
+        // Testing Damage
+        //======================================================================
+        System.out.println("Testing Damage");
+        System.out.println("================================================================================");
+        ArrayList<WeaponType> weapon_type_list = new ArrayList<WeaponType>(0);
+        weapon_type_list.add(WeaponType.LASER);
+        weapon_type_list.add(WeaponType.MISSILE);
+        Damage test_damage_a = new NumericDamage(3, 2);
+        Damage test_damage_b = new SpecificDamage(weapon_type_list, 2);
+        System.out.println("First damage: " + test_damage_a.toString());
+        System.out.println("Second damage: " + test_damage_b.toString());
 
-			puts "Testing discardShieldBooster()"
-			10.times do
-					numeric_damage_test.discardShieldBooster
-			end
-			puts "Has no effect: #{numeric_damage_test.hasNoEffect}"
-			puts ""
+        // Testing copies
+        Damage test_damage_a_copy = test_damage_a.copy();
+        Damage test_damage_b_copy = test_damage_b.copy();
+        System.out.println("The copy of the first damage is: " + test_damage_a_copy);
+        System.out.println("The copy of the second damage is: " + test_damage_b_copy);
 
-			puts "Testing discardWeapon"
-			10.times do
-					numeric_damage_test.discardWeapon("trash")
-			end
-			puts "Has no effect: #{numeric_damage_test.hasNoEffect}"
-			puts ""
-		end
+        // Testing the adjust method
+        System.out.println("TESTING Damage.adjust()");
+        
+        ArrayList<Weapon> weapons_to_adjust = new ArrayList<>(0);
+        weapons_to_adjust.add(new Weapon("first weapon", WeaponType.LASER, 3));
+        
+        ArrayList<ShieldBooster> shields_to_adjust = new ArrayList<ShieldBooster>();
+        shields_to_adjust.add(new ShieldBooster("shield to adjust", (float)2.3134, 4));        
+        shields_to_adjust.add(new ShieldBooster("shield to adjust", (float)2.3134, 4));
+ 
+        Damage adjusted = test_damage_a.adjust(weapons_to_adjust, shields_to_adjust);
+        System.out.println("First adjusted damage is " + adjusted.toString());
+        adjusted = test_damage_b.adjust(weapons_to_adjust, shields_to_adjust);
+        System.out.println("Second adjusted damage is " + adjusted.toString());
 
-		puts "TEST CLASS: EnemyStarShip"
-		puts "================================================================================"
-		
-		puts "Creating an enemy starship"
-		enemy_test = EnemyStarShip.new("enemy tank ship", 1.123, 2.123124, Loot.new(1,2,3,4,5), Damage.newNumericWeapons(3,3))
-		puts "Enemy starship: #{enemy_test}"
-		puts ""
+        // Testing discard methods
+        System.out.println("Discarding some weapons");
+        for(int i = 0; i < 4; i++){
+            System.out.println("Discarding " + i + " weapon on first damage");     
+            test_damage_a.discardWeapon(test_weapon);
+            System.out.println("Discarding " + i + " weapon on second damage");
+            test_damage_b.discardWeapon(test_weapon);
+        }
 
-		puts "Creating a copy of the enemy starship"
-		enemy_copy = EnemyStarShip.newCopy(enemy_test)
-		puts "Copy: #{enemy_copy}"
-		puts ""
+        System.out.println("Discarding some shields");
+        for(int i = 0; i < 4; i++){
+            System.out.println("Discarding " + i + " shield on first damage");
+            test_damage_a.discardShieldBooster();
+            System.out.println("Discarding " + i + " shield on second damage");
+            test_damage_b.discardShieldBooster();
+        }
 
-		puts "Creating an UI version of the ship"
-		enemy_UI = enemy_test.getUIVersion
-		puts "UI version: #{enemy_UI}"
-		puts ""
+        System.out.println("State of the first damage: " + test_damage_a.toString());
+        System.out.println("State of the second damage: " + test_damage_b.toString());
 
-		puts "Protecting: #{enemy_test.protection}"
-		puts ""
+        System.out.println("");
+        System.out.println("");
 
-		puts "Firing: #{enemy_test.fire}"
-		puts ""
+        // Testing EnemyStarShip
+        //======================================================================
+        System.out.println("Testing EnemyStarShip");
+        System.out.println("================================================================================");
+        Damage test_damage = new NumericDamage(2, 3);
+        Loot test_loot = new Loot(1, 2, 3, 4, 5, false, false);
+        
+        // Testing the constructor
+        EnemyStarShip test_enemy = new EnemyStarShip("Enemy Test", (float)1.1, (float)2.2, test_loot, test_damage);
+        System.out.println("The testing EnemyStarShip is " + test_enemy.toString());
 
-		puts "Testing EnemyStarShip.receiveShot()"
-		10.times do |power|
-				puts "\tAttacking with #{power} power!"
-				result = enemy_test.receiveShot(power)
-				puts "\tResult: #{result}"
-		end
+        // Testing the copy constructor
+        EnemyStarShip test_enemy_copy = new EnemyStarShip(test_enemy);
+        System.out.println("The copy of the enemy is " + test_enemy_copy.toString());
 
-		# WIP -- Not tested properly -- Practica 3
-		puts "TEST CLASS: Space Station"
-		puts "================================================================================"
+        // Testing the UI version
+        System.out.println("The UI version is " + test_enemy.getUIversion().toString());
 
-		puts "Creating an Space Station"
-		space_test = SpaceStation.new("my tanky space station", SuppliesPackage.new(14, 12, 40))
-		puts "Space Station: #{space_test}"
-		puts ""
-
-		puts "Testing speed"
-		puts "Speed: #{space_test.speed}"
-		puts "Space station state: #{space_test}"
-		puts "Valid state: #{space_test.validState}"
-		puts ""
-
-		puts "Testing move"
-		space_test.move
-		puts "Space station state: #{space_test}"
-
-		puts "Testing receive Hangar"
-		space_test.receiveHangar(Hangar.new(5))
-		puts "Space station: #{space_test}"
-		puts ""
-
-		puts "Testing UI version"
-		puts "UI: #{space_test.getUIVersion}"
-		puts ""
-
-
-		# WIP -- Not tested properly -- Practica 3
-		puts "TEST CLASS: GameUniverse"
-		puts "================================================================================"
-		puts "Creating a Game universe"
-		universe_test = GameUniverse.new
-		puts "Game Universe: #{universe_test}"
-		puts ""
-
-		puts "Testing getters:"
-		puts "\tGame State: #{universe_test.gameState}"
-		puts ""
-
-		puts "Have a winner: #{universe_test.haveAWinner}"
-		puts ""
-    }
-*/
+        // Testing some shots
+        System.out.println("The enemy is going to receive some shots");
+        for(int i = 0; i < 10; i++){
+            ShotResult shotResult = test_enemy.receiveShot(i);
+            System.out.println("Receiving " + i + " power shot results in " + shotResult);
+        }
+        
+        System.out.println("");
+        System.out.println("");
+    } 
 }
